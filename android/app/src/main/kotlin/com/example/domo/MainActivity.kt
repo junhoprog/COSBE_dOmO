378dcb3cbc40b258cd5c210145a32450c533ab40
김준호 <kimjunho1231@naver.com>
2022-10-10 13:56:12 +0900

    Combined pages

--- android/app/src/main/kotlin/com/example/domo/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.domo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/dogam_page/variable/dogam_variable.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-PageController  dogam_album_controller= new PageController(initialPage: 0);
-PageController dogam_do_album_controller = new PageController(initialPage: 0);
-PageController dogam_si_album_controller = new PageController(initialPage: 0);
-PageController dogam_upload_controller = new PageController(initialPage: 0);
